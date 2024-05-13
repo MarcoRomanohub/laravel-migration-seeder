5e2f8e434f624e6a3a4d561439f7d02025010104
MarcoRomanoHub <marcoromano270@gmail.com>
2024-05-13 15:54:39 +0200

    [add] columns

--- database/migrations/2024_05_13_131821_create_trains_table.php
+++ database/migrations/2024_05_13_131821_create_trains_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 70);
+            $table->string('slug', 100)->unique();
+            $table->string('departure_station', 70);
+            $table->string('arrival_station', 70);
+            $table->decimal('departure_time', 4, 2)->unsigned();
+            $table->decimal('arrival_time', 4, 2)->unsigned();
+            $table->integer('code');
+            $table->tinyInteger('number_of_carriages');
+            $table->boolean('in_time')->default(true);
+            $table->boolean('deleted')->default(false);
             $table->timestamps();
         });
     }
