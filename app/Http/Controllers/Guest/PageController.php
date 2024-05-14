cd1180d82795f7279731bbeb99b7bc684f96c598
MarcoRomanoHub <marcoromano270@gmail.com>
2024-05-14 17:53:43 +0200

    [add] faker seeder

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,14 +4,19 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Train;
+
 
 class PageController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('home');
     }
 
-    public function nuovaPagina(){
-        return view('nuova-pagina');
+    public function trains()
+    {
+        $trains = Train::all();
+        return view('trains', compact('trains'));
     }
 }
--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -6,44 +6,60 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
 use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
 
-
-        $data = [
-            [
-                "company" => 'Trenitalia',
-                "slug" => 'Trenitalia',
-                "departure_station" => 'Roma Termini',
-                "arrival_station" => 'Milano Centrale',
-                "departure_time" => 12.30,
-                "arrival_time" => 17.00,
-                "code" => '123456789012',
-                "number_of_carriages" => '12',
-                "deleted" => false
-            ]
-        ];
-
-        foreach ($data as $item) {
+        for ($i = 0; $i < 10; $i++) {
             $new_train = new Train();
-            $new_train->company = $item['company'];
+            $new_train->company = $faker->words(3, true);
             $new_train->slug = $this->generateSlug($new_train->company);
-            $new_train->departure_station = $item['departure_station'];
-            $new_train->arrival_station = $item['arrival_station'];
-            $new_train->departure_time = $item['departure_time'];
-            $new_train->arrival_time = $item['arrival_time'];
-            $new_train->code = $item['code'];
-            $new_train->number_of_carriages = $item['number_of_carriages'];
-            $new_train->deleted = $item['deleted'];
+            $new_train->departure_station = $faker->words(1, true);
+            $new_train->arrival_station = $faker->words(1, true);
+            $new_train->departure_time = $faker->time();;
+            $new_train->arrival_time = $faker->time();;
+            $new_train->code =  $faker->randomNumber(9, true);;
+            $new_train->number_of_carriages = $faker->randomDigitNotNull();
+            $new_train->deleted = $faker->numberBetween(0, 1);
 
-            // $new_train-> save();
+            $new_train->save();
         }
+
+
+        // $data = [
+        //     [
+        //         "company" => 'Trenitalia',
+        //         "slug" => 'Trenitalia',
+        //         "departure_station" => 'Roma Termini',
+        //         "arrival_station" => 'Milano Centrale',
+        //         "departure_time" => 12.30,
+        //         "arrival_time" => 17.00,
+        //         "code" => '123456789012',
+        //         "number_of_carriages" => '12',
+        //         "deleted" => false
+        //     ]
+        // ];
+
+        // foreach ($data as $item) {
+        //     $new_train = new Train();
+        //     $new_train->company = $item['company'];
+        //     $new_train->slug = $this->generateSlug($new_train->company);
+        //     $new_train->departure_station = $item['departure_station'];
+        //     $new_train->arrival_station = $item['arrival_station'];
+        //     $new_train->departure_time = $item['departure_time'];
+        //     $new_train->arrival_time = $item['arrival_time'];
+        //     $new_train->code = $item['code'];
+        //     $new_train->number_of_carriages = $item['number_of_carriages'];
+        //     $new_train->deleted = $item['deleted'];
+
+        //     // $new_train-> save();
+        // }
 
 
         // $new_train = new Train();
@@ -59,7 +75,6 @@ class TrainsTableSeeder extends Seeder
         // $new_train->deleted = false;
 
 
-        dump($new_train);
     }
 
     private function generateSlug($string)
@@ -67,7 +82,7 @@ class TrainsTableSeeder extends Seeder
         $slug = Str::slug($string, '-');
         $original_slug = $slug;
 
-        $exists = Train::where('slug', $slug)->first;
+        $exists = Train::where('slug', $slug)->first();
         $c = 1;
 
         while ($exists) {
